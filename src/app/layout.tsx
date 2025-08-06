a291235b572060edb981ec8d9fd22b20395b88ad
duyicheng <260316514@qq.com>
2025-08-06 08:58:12 +0800

    head,添加追踪

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,6 +78,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-8082CH0NRB"
+        />
+        <script
+          id="google-analytics"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-8082CH0NRB');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
